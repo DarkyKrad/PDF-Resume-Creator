ff7577ececde559c12f5493c276740723dbc87c7
John Carlo Alam <104263926+DarkyKrad@users.noreply.github.com>
2022-07-09 20:30:47 +0800

    First commit

---
+++ PDF Resume Creator/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace PDF_Resume_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFP
+            // 
+            this.btnFP.Location = new System.Drawing.Point(125, 180);
+            this.btnFP.Name = "btnFP";
+            this.btnFP.Size = new System.Drawing.Size(86, 22);
+            this.btnFP.TabIndex = 0;
+            this.btnFP.Text = "Open File";
+            this.btnFP.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 420);
+            this.Controls.Add(this.btnFP);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnFP;
+    }
+}
